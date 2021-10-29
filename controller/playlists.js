f570616563bb18f53dde7a1ee6361d2956bced26
Eduardo R. Müller <a01745219@itesm.mx>
2021-10-29 07:33:03 -0600

    Controllers avanzado, solo falta revisar que los strings de titulo y descripcion cumplan con la validacion (size)

--- controller/playlists.js
+++ controller/playlists.js
@@ -1,18 +1,37 @@
 const mongoose = require('mongoose')
 const PlaylistsSchema = require('../models/playlists')
 
-exports.agregarPlaylist = () => {
-
+exports.agregarPlaylist = async (request, response) => {
+    const newPlaylist = new PlaylistsSchema(request.body)
+    try{
+        await newPlaylist.save()
+        console.log('Agregado')
+        response.json({operacion: 'correcta'})
+    }catch(error){
+        console.log(err)
+        res.json({operacion: "incorrecta"})
+    }
 }
 
-exports.obtenerPlaylists = () => {
-
+exports.obtenerPlaylists = async (request, response) => {
+    const playlists = await PlaylistsSchema.find()
+    console.log(playlists)
+    response.json(playlists)
 }
 
-exports.actualizarPlaylists = () => {
-
+exports.actualizarPlaylists = async (request, response) => {
+    try{
+        await PlaylistsSchema.findOneAndUpdate(request.body.filtro, request.body.cambio)
+        console.log('Cambio realizado')
+        response.json({operacion: 'correcta'})
+    }catch(error){
+        console.log(err)
+        res.json({operacion: "incorrecta"})
+    }
 }
 
-exports.borrarPlaylist = () => {
-
+exports.borrarPlaylist = async (request, response) => {
+    await PlaylistsSchema.findByIdAndRemove(req.body)
+    console.log("Playlist eliminada")
+    res.json({operacion: "correcta"})
 }
